0a6c9058aec7b322b4fcd66dff9cf2bd160ccd92
Tumaini <tumainikebaya.234@gmail.com>
2024-06-01 15:12:08 +0300

    Adds About.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,12 +7,14 @@ import About from "./About";
 import user from "../data/user";
 console.log(user);
 
+
+
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} city={user.city} color={user.color} />
+      <About bio={user.bio} github={user.links.github} linkedin={user.links.linkedin} />
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React from "react";
+//import user from "../data/user";
+
 
 function Home(props) {
   return (
